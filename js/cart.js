50547878b94f8976f0d81c0de66209866a041099
Andriy Trishechkin <andrej122001@gmail.com>
2023-05-26 13:32:00 +0300

    #1.1 Create validator

--- js/cart.js
+++ js/cart.js
@@ -120,31 +120,82 @@ const clearData = () => {
 cartInfo.addEventListener('submit', e => {
     e.preventDefault()
     if (localStorage.getItem('cart')) {
-        fetch('https://delivery-app-c64ab-default-rtdb.firebaseio.com/orders.json', {
-            method: 'POST',
-            body: JSON.stringify(createOrder())
-        })
-            .then(response => {
-                if (response.ok) {
-                    done.style.opacity = '1'
-                    setTimeout(() => {
-                        done.style.opacity = '0'
-                    }, 2000)
-                    clearData()
-                    getPostCart()
-                }
-                else {
-                    cross.style.opacity = '1'
-                    setTimeout(() => {
-                        cross.style.opacity = '0'
-                    }, 2000)
-                }
+        if (require(userName) && require(userEmail) && require(userPhone)) {
+            fetch('https://delivery-app-c64ab-default-rtdb.firebaseio.com/orders.json', {
+                method: 'POST',
+                body: JSON.stringify(createOrder())
             })
+                .then(response => {
+                    if (response.ok) {
+                        done.style.opacity = '1'
+                        setTimeout(() => {
+                            done.style.opacity = '0'
+                        }, 2000)
+                        clearData()
+                        getPostCart()
+                    }
+                    else {
+                        cross.style.opacity = '1'
+                        setTimeout(() => {
+                            cross.style.opacity = '0'
+                        }, 2000)
+                    }
+                })
+        }
     }
     else {
         alert('Cart is empty')
     }
 })
+
+const validName = /^([A-Z]{1}[a-z]{1,23})$/;
+const validEmail = /^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/
+const validPhone = /^(\+?38)?(0\d{9})$/
+
+class Validator {
+    isName(name) {
+        return validName.test(name)
+    }
+    isEmail(email) {
+        return validEmail.test(email)
+    }
+    isPhone(phone) {
+        return validPhone.test(phone)
+    }
+    isRequire(require) {
+        if (require.trim() === '') return false
+        else return true
+    }
+}
+
+const validate = new Validator()
+
+const require = (input) => {
+    if (validate.isRequire(input.value)) {
+        switch (input) {
+            case userName:
+                return compare(validate.isName(input.value), input)
+            case userEmail:
+                return compare(validate.isEmail(input.value), input)
+            case userPhone:
+                return compare(validate.isPhone(input.value), input)
+        }
+    }
+    else {
+        compare(false, input)
+    }
+}
+
+const compare = (isValid, input) => {
+    if (isValid) {
+        input.style.border = "3px solid green"
+        return true
+    }
+    else {
+        input.style.border = "3px solid red"
+        return false
+    }
+}
 
 getPostCart()
 
